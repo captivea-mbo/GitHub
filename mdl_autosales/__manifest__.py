57ebf60ac7e0085e2a37b26b0666a16187124244
Marcos Bolivar <66689110+captivea-mbo@users.noreply.github.com>
2020-08-28 17:14:35 -0400

    autosales

---
+++ mdl_autosales/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Autosales',
+    'version': '12.0.0.0',
+    'summary': """Module Tester for Autosaleso""",
+    'author': 'Marcos Bolivar',
+    'category': 'Extra Tools',           
+    'depends': [],
+    'images': [],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
